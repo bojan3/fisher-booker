59c9296f1d7780a3f5d33ba91321f292e7ecbdc2
bojan3 <bojanskokicxd@outlook.com>
2021-10-23 16:50:01 +0200

    spring boot init

---
+++ src/main/java/com/example/fisherbooker/FisherBookerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fisherbooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisherBookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisherBookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fisherbooker/FisherBookerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fisherbooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisherBookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
