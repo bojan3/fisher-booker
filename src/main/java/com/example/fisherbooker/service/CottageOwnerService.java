5263427d663d8c15c9cef2afdda78a295e8056e4
J-Gaspar99 <jbutea@gmail.com>
2021-11-14 18:22:29 +0100

    RegistrationController za registraciju

---
+++ src/main/java/com/example/fisherbooker/service/CottageOwnerService.java
@@ -0,0 +1,19 @@
+package com.example.fisherbooker.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.fisherbooker.repository.CottageOwnerRepository;
+import com.example.fisherbooker.repository.ShipOwnerRepository;
+
+@Service
+public class CottageOwnerService {
+	
+		public CottageOwnerRepository cottageownerrepository;
+
+		@Autowired
+		public CottageOwnerService(CottageOwnerRepository cottageownerRepository) {
+			this.cottageownerrepository = cottageownerRepository;
+		}
+	}
+
