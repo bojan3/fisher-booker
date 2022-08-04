9d2f61bdad500e7081106f88db96346845cd459f
bojan3 <bojanskokicxd@outlook.com>
2022-08-04 17:56:52 +0200

    merge complaint

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -70,15 +70,16 @@ insert into account_status(account_id,status_id)values(1,1);
 insert into account_status(account_id,status_id)values(2,2);
 insert into account_status(account_id,status_id)values(3,3);
 insert into account_status(account_id,status_id)values(4,4);
-insert into account_status(account_id,status_id)values(5,5);
-insert into account_status(account_id,status_id)values(6,6);
-insert into account_status(account_id,status_id)values(7,7);
-insert into account_status(account_id,status_id)values(8,8);
-insert into account_status(account_id,status_id)values(9,9);
-insert into account_status(account_id,status_id)values(10,10);
-insert into account_status(account_id,status_id)values(11,11);
-insert into account_status(account_id,status_id)values(12,12);
-insert into account_status(account_id,status_id)values(13,13);
+-- pravi problem negde u nekom od ovih dodela statusa
+--insert into account_status(account_id,status_id)values(5,5);
+--insert into account_status(account_id,status_id)values(6,6);
+--insert into account_status(account_id,status_id)values(7,7);
+--insert into account_status(account_id,status_id)values(8,8);
+--insert into account_status(account_id,status_id)values(9,9);
+--insert into account_status(account_id,status_id)values(10,10);
+--insert into account_status(account_id,status_id)values(11,11);
+--insert into account_status(account_id,status_id)values(12,12);
+--insert into account_status(account_id,status_id)values(13,13);
 
 
 
